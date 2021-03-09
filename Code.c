a5304bae8ed83bfea4f0d947a97d9e4f1ca9c4ab
Ege  Erbilen <grbln@hotmail.com>
2021-03-09 19:15:34 +0300

    first commit

--- Code.c
+++ Code.c
@@ -1,31 +1,25 @@
 #include "stm32f10x.h"
-void SysTick_Handler(void){
-	if(GPIOC -> ODR | (0<<13)){
-		GPIOC -> ODR &=~ (1<<13);
-	}
-	else {
-		GPIOC -> ODR |= (1<<13);
-	}
-}
-void EXTI15_10_IRQHandler(void){
-	if((EXTI -> PR & (1<<14)) != 0){
-		SysTick -> LOAD = 50*(72000000/8000);       
-		SysTick -> CTRL=3;
-		EXTI -> PR |= (1<<14);
-	}
-}
 int main()
 {
-	RCC -> APB2ENR |= ((1<<4)|1); 
-	GPIOC-> CRH |= ((1<<27));
-	GPIOC -> CRH &=~ ((1<<24)|(1<<25)|(1<<26));
-	GPIOC -> CRH |= ((1<<20)|(1<<21));
-	GPIOC -> CRH &=~ ((1<<23)|(1<<22));
-	AFIO->EXTICR[3] |= 1<<9;
-	AFIO->EXTICR[3] &=~ ((1<<11)|(1<<10)|(1<<8)); 
-	EXTI->FTSR |= 1<<14; 
-	EXTI->RTSR |= 1 <<14; 
-	EXTI->IMR |= 1<<14; 
-	NVIC_EnableIRQ(EXTI15_10_IRQn);  
-	while(1){ }
+		RCC -> APB2ENR |= (1<<4); 
+		GPIOC -> CRH |=(1<<27);	
+		GPIOC -> CRH &=~((1<<26)|(1<<25)|(1<<24)); 
+		
+		GPIOC -> CRH |=((1<<21)|(1<<20));	
+		GPIOC -> CRH &=~((1<<23)|(1<<22));	
+
+		GPIOC -> ODR |= (1<<13);  
+    while(1){				
+        while(!(GPIOC->IDR&(1<<14)));
+        while(GPIOC->IDR&(1<<14));
+        for(int i=0;i<5;i++)
+        {
+					GPIOC -> ODR |=(1<<13);	
+					for(int j = 0; j<2000000 ;j++); 
+					GPIOC -> ODR &=~(1<<13); 
+					for(int j= 0; j<2000000 ;j++);
+        }
+			GPIOC -> ODR |= (1<<13); 
+
+    }
 }
